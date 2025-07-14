bbb89a6d6b8d4496dfaa46a19e51040e09b77006
Christian Yves <fotechys@gmail.com>
2025-07-14 20:01:30 +0100

    working on docker compose

--- src/redis_client.py
+++ src/redis_client.py
@@ -1,9 +1,6 @@
 import aioredis
-import ssl
 from src.config import settings
 
-ssl_context = ssl.create_default_context()
-# Pour ignorer les certificats non signés (non recommandé pour prod)
-ssl_context.check_hostname = False
-ssl_context.verify_mode = ssl.CERT_NONE
-redis = aioredis.from_url(settings.REDIS_CACHE_URL, ssl=ssl_context,decode_responses=True)
+
+
+redis = aioredis.from_url(settings.REDIS_CACHE_URL,decode_responses=True)
