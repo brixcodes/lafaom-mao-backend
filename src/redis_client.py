68d07246635e3f0bbf691997a08ad6560bda7e2b
Christian Yves <fotechys@gmail.com>
2025-07-15 10:27:08 +0100

    working on redis and celery

--- src/redis_client.py
+++ src/redis_client.py
@@ -3,14 +3,14 @@ import ssl
 from src.config import settings
 
 
-
-
-
 def get_redis():
+    ssl_context = ssl.create_default_context()
+    ssl_context.check_hostname = False
+    ssl_context.verify_mode = ssl.CERT_REQUIRED 
     
     return Redis.from_url(
         settings.REDIS_CACHE_URL,
-        ssl_cert_reqs=ssl.CERT_REQUIRED
+        ssl_context=ssl_context
     )
 
 async def set_to_redis(key, value, ex=None):
