a97d0841ea94b24c846f1034891947c8ed23ef8f
Christian Yves <fotechys@gmail.com>
2025-07-15 08:49:20 +0100

    working on connection of edis

--- src/redis_client.py
+++ src/redis_client.py
@@ -1,6 +1,12 @@
-import aioredis
+from redis.asyncio import Redis
+import ssl
 from src.config import settings
 
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = False
+ssl_context.verify_mode = ssl.CERT_NONE
 
 
-redis = aioredis.from_url(settings.REDIS_CACHE_URL)
+
+
+redis = Redis.from_url(settings.REDIS_CACHE_URL,ssl=ssl_context)
