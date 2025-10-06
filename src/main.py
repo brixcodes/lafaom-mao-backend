402c92cf45d02fdf92056c75eb3885ffb829e716
lafaom <nanyangbrice.devops@gmail.com>
2025-10-06 11:54:01 +0100

    accept all origins

--- src/main.py
+++ src/main.py
@@ -108,17 +108,7 @@ async def validation_exception_handler(request: Request, exc: HTTPException):
         
 
 
-origins = [
-    "http://localhost",
-    "https://vitrine-lafaom.vercel.app",
-    "http://localhost:4200",
-    "http://127.0.0.1:4200",
-    "http://127.0.0.1:5500",
-    "http://localhost:5173",
-    "https://www.lafaom.vertex-cam.com/",
-    "https://lafaom.vertex-cam.com",
-    
-]
+origins = ["*"]
 
 @app.get("/", tags=["Root"])
 async def root() -> dict:
